328bdeecbccdf3a713ba513620dee9e66c23a35b
Bokai Wang <jacksonkai0408@gmail.com>
2023-11-26 04:15:10 -0700

    Detach photos from items

--- code/app/src/main/java/com/example/javenture/HouseHoldItemRepository.java
+++ code/app/src/main/java/com/example/javenture/HouseHoldItemRepository.java
@@ -7,8 +7,10 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.util.Pair;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -573,5 +575,22 @@ public class HouseHoldItemRepository {
                         Log.w("db", "Error updating documents", e);
                     }
                 });
+    }
+
+    public void getPhotoUrlsOfItem(String id, OnSuccessListener<List<String>> listener) {
+        if (authService.getCurrentUser() == null) {
+            return;
+        }
+        DocumentReference docRef = db.collection("users").document(authService.getCurrentUser().getUid()).collection("items").document(id);
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    listener.onSuccess((List<String>) task.getResult().get("photoUrls"));
+                } else {
+                    Log.d("db", "get failed with ", task.getException());
+                }
+            }
+        });
     }
 }
