4b57fb8ae41246a77c143cca4f09e5aca8d425a9
wellszhang666 <1923768987@qq.com>
2023-12-04 03:55:13 -0700

    Add unit tests

---
+++ code/app/src/test/java/com/example/javenture/ImageItemTest.java
@@ -0,0 +1,46 @@
+package com.example.javenture;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import android.net.Uri;
+
+import org.junit.Test;
+
+public class ImageItemTest {
+
+    @Test
+    public void testSetLocalUri() {
+        Uri mockUri = mock(Uri.class);
+
+        ImageItem item = new ImageItem("url");
+        assertNull(item.getLocalUri());
+        item.setLocalUri(mockUri);
+        assertNull(item.getRemoteUrl());
+        assertEquals(mockUri, item.getLocalUri());
+    }
+
+    @Test
+    public void testSetRemoteUrl() {
+        Uri mockUri = mock(Uri.class);
+
+        ImageItem item = new ImageItem(mockUri);
+        assertNull(item.getRemoteUrl());
+        item.setRemoteUrl("url");
+        assertNull(item.getLocalUri());
+        assertEquals("url", item.getRemoteUrl());
+    }
+
+    @Test
+    public void testIsLocal() {
+        Uri mockUri = mock(Uri.class);
+        ImageItem localItem = new ImageItem(mockUri);
+        assertTrue(localItem.isLocal());
+        ImageItem remoteItem = new ImageItem("url");
+        assertFalse(remoteItem.isLocal());
+    }
+}
